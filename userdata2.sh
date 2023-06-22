ef28b69cb0c8363dc58a4b3b8022aaca085af6ed
Vamsi Chunduru <cvamsikrishna11@gmail.com>
2023-06-22 06:29:04 +0530

    added user data

---
+++ userdata2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Update the system
+sudo yum update -y
+
+# Install Apache HTTP Server (httpd)
+sudo yum install httpd -y
+
+# Install Git
+sudo yum install git -y
+
+
+# Clone the repository
+git clone https://github.com/cvamsikrishna11/ecommerce-web-app.git
+
+# Copy the files inside the cloned folder to the desired location
+sudo cp -r ecommerce-web-app/server2/* /var/www/html/
+
+# Start and enable the HTTP server
+sudo systemctl start httpd
+sudo systemctl enable httpd

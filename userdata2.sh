2afcc92470e0cbe74cf70c0665e70ad3d1527b12
Vamsi Chunduru <cvamsikrishna11@gmail.com>
2023-06-27 20:59:06 +0530

    added modifications to get the current instance ip from where we are getting the response from

--- userdata2.sh
+++ userdata2.sh
@@ -16,6 +16,10 @@ git clone https://github.com/cvamsikrishna11/ecommerce-web-app.git
 # Copy the files inside the cloned folder to the desired location
 sudo cp -r ecommerce-web-app/server2/* /var/www/html/
 
+# To get the current hostname address
+echo "<center><p>Running the website from instance host: $(hostname -f)</p></center>" >> /var/www/html/index.html
+
+
 # Start and enable the HTTP server
 sudo systemctl start httpd
 sudo systemctl enable httpd

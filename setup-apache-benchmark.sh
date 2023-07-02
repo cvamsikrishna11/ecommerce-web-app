2282bc92621da500bcc80dc840fa954f60d70e18
Vamsi Chunduru <cvamsikrishna11@gmail.com>
2023-07-02 15:08:23 +0530

    apache benchmark

--- setup-apache-benchmark.sh
+++ setup-apache-benchmark.sh
@@ -5,7 +5,7 @@ docker run -dit --name httpd-ab -v /var/www/html:/usr/local/apache2/htdocs/ http
 docker exec -it httpd-ab bash
 
 # To generate the load to the load balancer DNS, replace load balancer DNS with your ALB DNS(EC2 console --> Loadbalancers --> Select the newly created ALB and copy the DNS)
-ab -r -c 500 -n 5000000 http://ecs-d-publi-h9d4ufps3s74-2106267235.us-east-1.elb.amazonaws.com/
+ab -r -c 500 -n 5000000 -s 600 http://amazon-demo-elb-751057938.us-east-1.elb.amazonaws.com/
 
 # To stop the apache bench processor (ctrl for windows, cmd for mac)
 ctrl/cmd + c
